4bc375ed50658e5b34a6d920b55d4b86322a58f4
riasat1998 <riasat1998@gmail.com>
2025-09-10 19:50:56 -0300

    fixed migration datatype issues

--- app/controllers/cvs_controller.ts
+++ app/controllers/cvs_controller.ts
@@ -1,4 +1,11 @@
-// import type { HttpContext } from '@adonisjs/core/http'
+import { HttpContext } from "@adonisjs/core/http";
 
 export default class CvsController {
+
+    async getCvs({auth, response}: HttpContext){
+        const user = auth.user
+        
+        await user?.load('cvs')
+        return response.ok({cvs: user?.cvs})
+    }
 }
