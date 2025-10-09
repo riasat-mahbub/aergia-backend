7197f29d346e4606b9bc8e18d482d03fd12470a2
riasat1998 <riasat1998@gmail.com>
2025-10-09 15:05:34 -0300

    updated reorder route

--- start/routes.ts
+++ start/routes.ts
@@ -32,7 +32,7 @@ router.group(() =>{
   router.post('/', [CvsController, 'create'])
   router.delete('/:id', [CvsController, 'delete'])
   router.put('/:id', [CvsController, 'update'])
-  router.put('/:id/reorder', [CvsController, 'reorder'])
+  router.put('/reorder', [CvsController, 'reorder'])
   router.get('/:id', [CvsController, 'read'])
 }).
 prefix('cv').
