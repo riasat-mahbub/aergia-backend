c3fcb4af6a538fd26932f35a0ac1f24d1862c938
riasat1998 <riasat1998@gmail.com>
2025-11-08 14:53:19 -0400

    added pupeteer config

--- app/services/PdfService.ts
+++ app/services/PdfService.ts
@@ -10,7 +10,19 @@ export class PdfService {
     
     const html = await this.generateHtml(formGroups, template)
     
-    const browser = await puppeteer.launch({ headless: true })
+    const browser = await puppeteer.launch({ 
+      headless: true,
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-accelerated-2d-canvas',
+        '--no-first-run',
+        '--no-zygote',
+        '--single-process',
+        '--disable-gpu'
+      ]
+    })
     const page = await browser.newPage()
     
     await page.setContent(html, { waitUntil: 'networkidle0' })
